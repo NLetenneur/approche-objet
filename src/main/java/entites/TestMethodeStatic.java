505a9b7130d334a9ffc4d5438b0db9da8ebbf9bc
Noémie Letenneur <n.letenneur@gmail.com>
2024-05-02 16:59:16 +0200

    TP4

---
+++ src/main/java/entites/TestMethodeStatic.java
@@ -0,0 +1,20 @@
+package entites;
+
+public class TestMethodeStatic {
+
+	public static void main(String[] args) {
+		String chaine = "12";
+		System.out.println(chaine);
+		int number =  Integer.parseInt(chaine);
+		System.out.println(number);
+		int otherNumber = Integer.valueOf(chaine);
+		System.out.println(otherNumber);
+		
+		int a = 86;
+		int b = 456; 
+		int supp = Integer.max(a, b);
+		
+		System.out.println(supp);
+	}
+
+}
