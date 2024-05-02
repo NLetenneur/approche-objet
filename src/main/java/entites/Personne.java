4a6f8efea826247f8352b951cb638d26d837cd1f
Noémie Letenneur <n.letenneur@gmail.com>
2024-05-02 11:24:24 +0200

    fin TP1

--- src/main/java/entites/Personne.java
+++
@@ -1,8 +0,0 @@
-package entites;
-
-public class Personne {
-	String nom;
-	String prénom;
-	AdressePostale adresse;
-
-}
--- src/main/java/entites/TestPersonne.java
+++ src/main/java/entites/TestPersonne.java
@@ -1,12 +1,27 @@
 package entites;
 
+import entites2.Personne;
+
 public class TestPersonne {
 
 	public static void main(String[] args) {
 		Personne moi = new Personne();
 		moi.nom="Letenneur";
 		moi.prénom="Noémie";
-		moi.adresse=TestAdressePostale;
+		moi.adresse=new AdressePostale();
+		moi.adresse.numeroRue=1;
+		moi.adresse.libelleRue="rue de Morlaix";
+		moi.adresse.codePostal=44800;
+		moi.adresse.ville="Saint Herblain";
+		
+		Personne autre = new Personne();
+		autre.nom="Doe";
+		autre.prénom="Jane";
+		autre.adresse = new AdressePostale();
+		autre.adresse.numeroRue=5;
+		autre.adresse.libelleRue="rue des Maréchaux";
+		autre.adresse.codePostal=44100;
+		autre.adresse.ville="Nantes";
 	}
 
 }
